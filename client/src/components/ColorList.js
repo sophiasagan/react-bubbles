8c8f141ffd64be9ef615729e7a9dd33da27682cb
sophiasagan <sophiabraddock82@gmail.com>
2020-03-20 19:30:58 -0400

    mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, setDependency }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -21,11 +23,33 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+          console.log('rees', res);
+          setDependency(true);
+        })
+      
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/colors/${colorToEdit.id}`, color)
+      .then(res => {
+        updateColors(colors.filter(item => item.id !== colorToEdit ))
+        setDependency(true);
+      })
+      .catch(err => console.log('This is an error!', err))
   };
+
+  const saveColor = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/colors", newColor)
+      .then(res => setDependency(true))
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className="colors-wrap">
@@ -82,6 +106,33 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+      <form onSubmit={saveColor}>
+            <legend>add color</legend>
+            <label>
+              color name:
+              <input
+                onChange={e =>
+                  setNewColor({ ...newColor, color: e.target.value })
+                }
+                value={newColor.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                onChange={e =>
+                  setNewColor({
+                    ...newColor,
+                    code: { hex: e.target.value }
+                  })
+                }
+                value={newColor.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">save</button>
+            </div>
+          </form>
     </div>
   );
 };
